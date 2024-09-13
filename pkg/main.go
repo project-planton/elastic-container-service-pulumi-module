6f75010ec9390fa429af16823a333f0f40aeae8d
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 02:52:07 +0530

    removed unneeded struct

--- pkg/main.go
+++ pkg/main.go
@@ -7,12 +7,8 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-type ResourceStack struct {
-	StackInput *elasticcontainerservice.ElasticContainerServiceStackInput
-}
-
-func (s *ResourceStack) Resources(ctx *pulumi.Context) error {
-	awsCredential := s.StackInput.AwsCredential
+func Resources(ctx *pulumi.Context, stackInput *elasticcontainerservice.ElasticContainerServiceStackInput) error {
+	awsCredential := stackInput.AwsCredential
 
 	//create aws provider using the credentials from the input
 	_, err := aws.NewProvider(ctx,
