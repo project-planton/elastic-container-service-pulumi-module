454719b1db39a354e662c85d272a4e51c4e3b102
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:21:45 +0530

    using project-planton apis

--- pkg/main.go
+++ pkg/main.go
@@ -1,13 +1,13 @@
 package pkg
 
 import (
+	elasticcontainerservicev1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/aws/elasticcontainerservice/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/elasticcontainerservice"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Resources(ctx *pulumi.Context, stackInput *elasticcontainerservice.ElasticContainerServiceStackInput) error {
+func Resources(ctx *pulumi.Context, stackInput *elasticcontainerservicev1.ElasticContainerServiceStackInput) error {
 	awsCredential := stackInput.AwsCredential
 
 	//create aws provider using the credentials from the input
