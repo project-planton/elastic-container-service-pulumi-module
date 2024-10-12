f78d28b7d4e9129c31e2193920322fc81d8aa9b0
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:52:30 +0530

    moved to project-planton

--- pkg/main.go
+++ pkg/main.go
@@ -1,7 +1,7 @@
 package pkg
 
 import (
-	elasticcontainerservicev1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/aws/elasticcontainerservice/v1"
+	elasticcontainerservicev1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/aws/elasticcontainerservice/v1"
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
